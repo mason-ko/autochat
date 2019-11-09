8e1efcbe5a6f437f98ce1ef83ca3bf08242cd300
ko mason <gym1029@gmail.com>
2019-11-09 15:02:59 +0900

    change hub, to 1:1 message

--- README.md
+++ README.md
@@ -3,3 +3,12 @@
 Blazor , .net core 3.0
 
 스터디 겸 하여 시작
+
+
+# Using #
+
+SQLLITE
+Google Transition API
+.net core 3.0
+blazor
+SignalR
--- src/autochat_blazorapp/autochat_blazorapp/Hubs/ChatHub.cs
+++ src/autochat_blazorapp/autochat_blazorapp/Hubs/ChatHub.cs
@@ -15,6 +15,7 @@ namespace autochat_blazorapp.Hubs
         /// Needs to be static as the chat is created dynamically a lot
         /// </remarks>
         private static readonly Dictionary<string, string> userLookup = new Dictionary<string, string>();
+        private static readonly Dictionary<string, string> userIdLookup = new Dictionary<string, string>();
 
         /// <summary>
         /// Send a message to all clients
@@ -22,9 +23,16 @@ namespace autochat_blazorapp.Hubs
         /// <param name="username"></param>
         /// <param name="message"></param>
         /// <returns></returns>
-        public async Task SendMessage(string username, string message)
+        public async Task SendMessage(string username, string message, string toId)
         {
-            await Clients.All.SendAsync("ReceiveMessage", username, message);
+            //1대1로 변경
+            if (userIdLookup.TryGetValue(toId, out string connectionId))
+            {
+                await Clients.Client(connectionId)?.SendAsync("ReceiveMessage", username, message);
+            }
+
+
+            //await Clients.All.SendAsync("ReceiveMessage", username, message);
         }
 
         /// <summary>
@@ -32,15 +40,18 @@ namespace autochat_blazorapp.Hubs
         /// </summary>
         /// <param name="username"></param>
         /// <returns></returns>
-        public async Task Register(string username)
+        public async Task Register(string userId)
         {
             var currentId = Context.ConnectionId;
             if (!userLookup.ContainsKey(currentId))
             {
                 // maintain a lookup of connectionId-to-username
-                userLookup.Add(currentId, username);
+                userLookup.Add(currentId, userId);
+
+                userIdLookup[userId] = currentId;
+
                 // re-use existing message for now
-                await Clients.AllExcept(currentId).SendAsync("ReceiveMessage", username, $"{username} joined the chat");
+                //await Clients.AllExcept(currentId).SendAsync("ReceiveMessage", userId, $"{userId} joined the chat");
             }
         }
 
@@ -50,7 +61,8 @@ namespace autochat_blazorapp.Hubs
         /// <returns></returns>
         public override Task OnConnectedAsync()
         {
-            Console.WriteLine("Connected");
+            
+            Console.WriteLine("---------------- Connected");
             return base.OnConnectedAsync();
         }
 
@@ -64,11 +76,20 @@ namespace autochat_blazorapp.Hubs
             Console.WriteLine($"Disconnected {e?.Message}");
             // try to get connection
             string id = Context.ConnectionId;
-            if (userLookup.TryGetValue(id, out string username))
+            if (userLookup.TryGetValue(id, out string userId))
             {
                 userLookup.Remove(id);
-                await Clients.AllExcept(Context.ConnectionId).SendAsync("ReceiveMessage", username, $"{username} has left the chat");
+
+                if (userIdLookup.ContainsKey(userId))
+                {
+                    userIdLookup.Remove(userId);
+                }
+
+                //await Clients.AllExcept(Context.ConnectionId).SendAsync("ReceiveMessage", username, $"{username} has left the chat");
             }
+
+
+
             await base.OnDisconnectedAsync(e);
         }
 
